4e87246ae46a50d7ad90fa0924eba35ee7261ca7
Sudhanshu Siddh <sudhanshu.15@gmail.com>
2017-11-09 17:58:15 -0500

    Vuforia marker based AR on Unity 3D

---
+++ Assets/vBtnScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vBtnScript : MonoBehaviour, IVirtualButtonEventHandler {
+
+	private GameObject animationButton;
+	private GameObject butterFly;
+	private GameObject rabbitDance;
+
+	// Use this for initialization
+	void Start () {
+
+		animationButton = GameObject.Find ("animationButton");
+		VirtualButtonBehaviour vb = animationButton.GetComponent<VirtualButtonBehaviour> ();
+		if (vb) {
+			vb.RegisterEventHandler (this);
+		}
+		butterFly = GameObject.Find ("butterfly");
+		rabbitDance = GameObject.Find ("rabbitDance");
+
+	}
+
+	public void OnButtonPressed(VirtualButtonBehaviour vb){
+
+		Debug.Log ("Button Pressed");
+		butterFly.GetComponent<Animation> ().Play ();
+		rabbitDance.GetComponent<Animation> ().Play ();
+
+	
+	}
+
+	public void OnButtonReleased(VirtualButtonBehaviour vb){
+
+		butterFly.GetComponent<Animation> ().Stop ();
+		rabbitDance.GetComponent<Animation> ().Stop ();
+	
+	}
+}
